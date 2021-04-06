d2d1e62428cc10faf6b72a9c50253aaa43e3e113
irena-sh <ira.shvecova@gmail.com>
2021-04-06 17:17:11 +0300

    First commit

---
+++ src/components/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: 'Marketing',
+    path: '/marketing',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Consulting',
+    path: '/consulting',
+    cName: 'dropdown-link',
+  },
+  {
+    title: 'Product',
+    path: '/product',
+    cName: 'dropdown-link',
+  }
+  // {
+  //   title: 'Design',
+  //   path: '/design',
+  //   cName: 'dropdown-link',
+  // },
+  // {
+  //   title: 'Development',
+  //   path: '/development',
+  //   cName: 'dropdown-link',
+  // }
+];
